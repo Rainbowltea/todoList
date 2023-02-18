7b3ad219408ce45964dd08750caee416c1a17488
XiangXuhuai <1506347180@qq.com>
2023-02-18 18:50:15 +0800

    实现用户功能

--- user/internal/repository/user.go
+++ user/internal/repository/user.go
@@ -1,7 +1,12 @@
 package repository
 
 import (
+	"errors"
+	"user/internal/service"
+	"user/pkg/util"
+
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -22,5 +27,54 @@ func (user *User) SetPassword(password string) error {
 		return err
 	}
 	user.PasswordDigest = string(bytes)
+	return nil
+}
+
+// 检验密码
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordDigest), []byte(password))
+	return err == nil
+}
+
+func (user *User) CheckUserExist(req *service.UserRequest) bool {
+	if err := DB.Where("user_name=?", req.UserName).First(&user).Error; err == gorm.ErrRecordNotFound {
+		return false
+	}
+	return true
+}
+
+func (user *User) ShowUserInfo(req *service.UserRequest) (err error) {
+	if exist := user.CheckUserExist(req); exist {
+		return nil
+	}
+	return errors.New("UserName Not Exist")
+}
+
+// 视图返回
+func BuildUser(item User) *service.UserModel {
+	userModel := service.UserModel{
+		UserID:   uint32(item.UserID),
+		NickName: item.NickName,
+		UserName: item.UserName,
+	}
+	return &userModel
+}
+
+func (*User) Create(req *service.UserRequest) error {
+	var user User
+	var count int64
+	DB.Where("user_name=?", req.UserName).Count(&count)
+	if count != 0 {
+		return errors.New("UserName Exist")
+	}
+	user = User{
+		UserName: req.UserName,
+		NickName: req.NickName,
+	}
+	_ = user.SetPassword(req.Password)
+	if err := DB.Create(&user).Error; err != nil {
+		util.LogrusObj.Error("Insert User Error:" + err.Error())
+		return err
+	}
 	return nil
 }
